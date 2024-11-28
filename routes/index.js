b1184b1bcb644b7ed86ee5209aa195f5447aea5d
anugrahp13 <anugrahprastyo13@gmail.com>
2024-11-28 11:39:36 +0700

    feat[index.js]: create file on folder routes for process Route API Get Data Users

--- routes/index.js
+++ routes/index.js
@@ -1,20 +1,34 @@
 // Import express
-const express = require('express');
+const express = require("express");
 
 // Init express router
 const router = express.Router();
 
 // Import validators and middleware
-const { validateLogin } = require('../utils/validators');
-const { handleValidationErrors } = require('../middlewares');
+const { validateLogin } = require("../utils/validators");
+const { handleValidationErrors, verifyToken } = require("../middlewares");
 
 // Import controllers
-const loginController = require('../controllers/LoginController');
+const loginController = require("../controllers/LoginController");
+const userController = require("../controllers/UserController");
 
 // Define routes
 const routes = [
   // Login route
-  { method: 'post', path: '/login', middlewares: [validateLogin, handleValidationErrors], handler: loginController.login },
+  {
+    method: "post",
+    path: "/login",
+    middlewares: [validateLogin, handleValidationErrors],
+    handler: loginController.login,
+  },
+
+  // User routes
+  {
+    method: "get",
+    path: "/users",
+    middlewares: [verifyToken],
+    handler: userController.findUsers,
+  },
 ];
 
 // Helper function to create routes
