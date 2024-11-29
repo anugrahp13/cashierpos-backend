f25e12b9b2e3e740821bda63eb8d95e2ad8c2c3b
anugrahp13 <anugrahprastyo13@gmail.com>
2024-11-29 21:22:11 +0700

    feat[index.js]: added routes product method post for path product by barcode

--- routes/index.js
+++ routes/index.js
@@ -1,46 +1,171 @@
 // Import express
-const express = require('express');
+const express = require("express");
 
 // Init express router
 const router = express.Router();
 
 // Import validators and middleware
-const { validateLogin, validateUser, validateCategory, validateProduct } = require('../utils/validators');
-const { handleValidationErrors, verifyToken, upload } = require('../middlewares');
+const {
+  validateLogin,
+  validateUser,
+  validateCategory,
+  validateProduct,
+} = require("../utils/validators");
+const {
+  handleValidationErrors,
+  verifyToken,
+  upload,
+} = require("../middlewares");
 
 // Import controllers
-const loginController = require('../controllers/LoginController');
-const userController = require('../controllers/UserController');
-const categoryController = require('../controllers/CategoryController');
-const productController = require('../controllers/ProductController');
+const loginController = require("../controllers/LoginController");
+const userController = require("../controllers/UserController");
+const categoryController = require("../controllers/CategoryController");
+const productController = require("../controllers/ProductController");
 
 // Define routes
 const routes = [
   // Login route
-  { method: 'post', path: '/login', middlewares: [validateLogin, handleValidationErrors], handler: loginController.login },
+  {
+    method: "post",
+    path: "/login",
+    middlewares: [validateLogin, handleValidationErrors],
+    handler: loginController.login,
+  },
 
   // User routes
-  { method: 'get', path: '/users', middlewares: [verifyToken], handler: userController.findUsers },
-  { method: 'post', path: '/users', middlewares: [verifyToken, validateUser, handleValidationErrors], handler: userController.createUser },
-  { method: 'get', path: '/users/:id', middlewares: [verifyToken], handler: userController.findUserById },
-  { method: 'put', path: '/users/:id', middlewares: [verifyToken, validateUser, handleValidationErrors], handler: userController.updateUser },
-  { method: 'delete', path: '/users/:id', middlewares: [verifyToken], handler: userController.deleteUser },
+  {
+    method: "get",
+    path: "/users",
+    middlewares: [verifyToken],
+    handler: userController.findUsers,
+  },
+  {
+    method: "post",
+    path: "/users",
+    middlewares: [verifyToken, validateUser, handleValidationErrors],
+    handler: userController.createUser,
+  },
+  {
+    method: "get",
+    path: "/users/:id",
+    middlewares: [verifyToken],
+    handler: userController.findUserById,
+  },
+  {
+    method: "put",
+    path: "/users/:id",
+    middlewares: [verifyToken, validateUser, handleValidationErrors],
+    handler: userController.updateUser,
+  },
+  {
+    method: "delete",
+    path: "/users/:id",
+    middlewares: [verifyToken],
+    handler: userController.deleteUser,
+  },
 
   // Category routes
-  { method: 'get', path: '/categories', middlewares: [verifyToken], handler: categoryController.findCategories },
-  { method: 'post', path: '/categories', middlewares: [verifyToken, upload.single('image'), validateCategory, handleValidationErrors], handler: categoryController.createCategory },
-  { method: 'get', path: '/categories/:id', middlewares: [verifyToken], handler: categoryController.findCategoryById },
-  { method: 'put', path: '/categories/:id', middlewares: [verifyToken, upload.single('image'), validateCategory, handleValidationErrors], handler: categoryController.updateCategory },
-  { method: 'delete', path: '/categories/:id', middlewares: [verifyToken], handler: categoryController.deleteCategory },
-  { method: 'get', path: '/categories-all', middlewares: [verifyToken], handler: categoryController.allCategories },
+  {
+    method: "get",
+    path: "/categories",
+    middlewares: [verifyToken],
+    handler: categoryController.findCategories,
+  },
+  {
+    method: "post",
+    path: "/categories",
+    middlewares: [
+      verifyToken,
+      upload.single("image"),
+      validateCategory,
+      handleValidationErrors,
+    ],
+    handler: categoryController.createCategory,
+  },
+  {
+    method: "get",
+    path: "/categories/:id",
+    middlewares: [verifyToken],
+    handler: categoryController.findCategoryById,
+  },
+  {
+    method: "put",
+    path: "/categories/:id",
+    middlewares: [
+      verifyToken,
+      upload.single("image"),
+      validateCategory,
+      handleValidationErrors,
+    ],
+    handler: categoryController.updateCategory,
+  },
+  {
+    method: "delete",
+    path: "/categories/:id",
+    middlewares: [verifyToken],
+    handler: categoryController.deleteCategory,
+  },
+  {
+    method: "get",
+    path: "/categories-all",
+    middlewares: [verifyToken],
+    handler: categoryController.allCategories,
+  },
 
   // Product routes
-  { method: 'get', path: '/products', middlewares: [verifyToken], handler: productController.findProducts },
-  { method: 'post', path: '/products', middlewares: [verifyToken, upload.single('image'), validateProduct, handleValidationErrors], handler: productController.createProduct },
-  { method: 'get', path: '/products/:id', middlewares: [verifyToken], handler: productController.findProductById },
-  { method: 'put', path: '/products/:id', middlewares: [verifyToken, upload.single('image'), validateProduct, handleValidationErrors], handler: productController.updateProduct },
-  { method: 'delete', path: '/products/:id', middlewares: [verifyToken], handler: productController.deleteProduct },
-  { method: 'get', path: '/products-by-category/:id', middlewares: [verifyToken], handler: productController.findProductByCategoryId },
+  {
+    method: "get",
+    path: "/products",
+    middlewares: [verifyToken],
+    handler: productController.findProducts,
+  },
+  {
+    method: "post",
+    path: "/products",
+    middlewares: [
+      verifyToken,
+      upload.single("image"),
+      validateProduct,
+      handleValidationErrors,
+    ],
+    handler: productController.createProduct,
+  },
+  {
+    method: "get",
+    path: "/products/:id",
+    middlewares: [verifyToken],
+    handler: productController.findProductById,
+  },
+  {
+    method: "put",
+    path: "/products/:id",
+    middlewares: [
+      verifyToken,
+      upload.single("image"),
+      validateProduct,
+      handleValidationErrors,
+    ],
+    handler: productController.updateProduct,
+  },
+  {
+    method: "delete",
+    path: "/products/:id",
+    middlewares: [verifyToken],
+    handler: productController.deleteProduct,
+  },
+  {
+    method: "get",
+    path: "/products-by-category/:id",
+    middlewares: [verifyToken],
+    handler: productController.findProductByCategoryId,
+  },
+  {
+    method: "post",
+    path: "/products-by-barcode",
+    middlewares: [verifyToken],
+    handler: productController.findProductByBarcode,
+  },
 ];
 
 // Helper function to create routes
